a6e771a814dd242b0ca3a0e8ed05bf1331127053
Manish Sharma <parasermanish@gmail.com>
2020-07-20 17:04:39 +0530

    Chef and An Ideal Problem

---
+++ CHFIDEAL/Main.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("3");
+		int SecondNumber = sc.nextInt();
+		if(SecondNumber==1){
+		    System.out.println("2");
+		}else{
+		    System.out.println("1");
+		}
+	}
+}
