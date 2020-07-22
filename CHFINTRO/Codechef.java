b65113b728d43c3d14ca337e6e93c1041f38f6b2
Manish Sharma <parasermanish@gmail.com>
2020-07-22 13:25:39 +0530

    Chef and Interactive Contests

---
+++ CHFINTRO/Codechef.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		int r=sc.nextInt();
+		while(t-->0)
+		{
+		    int R=sc.nextInt();
+		    if(R>=r)
+		    {
+		        System.out.println("Good boi");
+		    }
+		    else if(R<=r)
+		    {
+		        System.out.println("Bad boi");
+		    }
+		}
+	}
+}
