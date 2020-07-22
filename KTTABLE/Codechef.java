f92b547e2d73f099a427ee03756e7f4b8696f484
Manish Sharma <parasermanish@gmail.com>
2020-07-22 13:29:47 +0530

    Kitchen Timetable

---
+++ KTTABLE/Codechef.java
@@ -0,0 +1,42 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int arr[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        arr[i]=sc.nextInt();
+		    }
+		    int arr2[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        arr2[i]=sc.nextInt();
+		    }
+		    int count=0;
+		    if(arr2[0]<=arr[0])
+		    {
+		        count+=1;
+		    }
+		    for(int i=0;i<n-1;i++)
+		    {
+		        if(arr2[i+1]<=Math.abs(arr[i]-arr[i+1]))
+		        {
+		            count++;
+		        }
+		    }
+		    System.out.println(count);
+		}
+	}
+}
