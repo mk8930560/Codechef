8f5d2cec4cd3269763fa81a9798278b6a5e27735
Manish Sharma <parasermanish@gmail.com>
2020-07-22 13:16:20 +0530

    Two vs Ten

---
+++ TWOVSTEN/Codechef.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int a=sc.nextInt();
+		    if(a%10==0)
+		    {
+		        System.out.println("0"); 
+		    }
+            else if(a%10==5)
+            {
+                System.out.println("1");
+            }
+            else
+            {
+                System.out.println("-1");
+            }
+		}
+	}
+}
