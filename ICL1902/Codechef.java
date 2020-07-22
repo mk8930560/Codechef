489c94adbc1c1834f17a0f4bdef491184baa93aa
Manish Sharma <parasermanish@gmail.com>
2020-07-22 14:01:57 +0530

    FlatLand

---
+++ ICL1902/Codechef.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int val=0;
+		    int count=0;
+		    while(n>0)
+		    {
+		        for(int i=0;i<=n;i++)
+		        {
+		            int j=i+1;
+		            if(i*i<=n && j*j<=n)
+		            {
+		                val=j*j;
+		            }
+		        }
+		        n=n-val;
+		        count++;
+		    }
+		    System.out.println(count);
+		}
+	}
+}
