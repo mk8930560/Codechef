64c0260327a5bd01171d35bf72def3a269f2c0c9
Manish Sharma <parasermanish@gmail.com>
2020-07-22 13:11:49 +0530

    Chef and Remissness

---
+++ REMISS/Codechef.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int GardOneCount=sc.nextInt();
+		    int GardTwoCount=sc.nextInt();
+		    int minimum=Math.max(GardOneCount,GardTwoCount);
+		    int maximum=GardOneCount+GardTwoCount;
+		    System.out.println(minimum+" "+maximum);
+		}
+	}
+}
