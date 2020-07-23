b67da13184f928bf8c7b2474b10d05981b57f11b
Manish Sharma <parasermanish@gmail.com>
2020-07-23 14:35:07 +0530

    Weird Modulo Problem

---
+++ EXUNA/Codechef.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    ArrayList<Integer> arr=new ArrayList<Integer>(n);
+        	for(int i=0;i<n;i++)
+        	{
+                int x=sc.nextInt();
+                arr.add(x);
+        	}
+            Collections.sort(arr);
+        	System.out.println(arr.get(0));
+		}
+	}
+}
