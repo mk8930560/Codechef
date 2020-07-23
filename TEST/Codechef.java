4ddf7c7b03430446e89c70a01aa9573332b94c14
Manish Sharma <parasermanish@gmail.com>
2020-07-23 14:25:06 +0530

    Life, the Universe, and Everything

---
+++ TEST/Codechef.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		while(true)
+		{
+		    int n=sc.nextInt();
+		    if(n==42)
+		    {
+		        break;
+		    }
+		    System.out.println(n);
+		}
+	}
+}
