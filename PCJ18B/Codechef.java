f6b2bc8831b7e947aa4a0a21005eb770b960bfcd
Manish Sharma <parasermanish@gmail.com>
2020-07-23 14:09:10 +0530

    Chef and Bored Games

---
+++ PCJ18B/Codechef.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int sum=0;
+		    for(int i=1;i<=n;i=i+2)
+		    {
+		        int k=n-i+1;
+		        sum+=(k*k);
+		    }
+		    System.out.println(sum);
+		}
+	}
+}
