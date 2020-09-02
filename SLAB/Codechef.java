017e09a290291e42de0c538f14e9d0814ea067b2
Manish Sharma <parasermanish@gmail.com>
2020-09-02 12:12:40 +0530

    Tax Slabs

---
+++ SLAB/Codechef.java
@@ -0,0 +1,59 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    while(t-->0)
+	    {
+	        long n=sc.nextLong();
+	        long tax=0;
+	        if(n<=250000)
+	        {
+	            tax+=0;
+	        }else if(n>250000 && n<=500000)
+	        {
+	            tax+=0.05*(n-250000);
+	        }else if(n>500000 && n<=750000)
+	        {
+	            tax+=0.10*(n-500000);
+	            tax+=0.05*(500000-250000);
+	        }else if(n>750000 && n<=1000000)
+	        {
+	            tax+=0.15*(n-750000);
+	            tax+=0.10*(750000-500000);
+	            tax+=0.05*(500000-250000);
+	        }else if(n>1000000&&n<=1250000)
+	        {
+	            tax+=0.20*(n-1000000);
+	            tax+=0.15*(1000000-750000);
+	            tax+=0.10*(750000-500000);
+	            tax+=0.05*(500000-250000);
+	        }else if(n>1250000&& n<=1500000)
+	        {
+	            tax+=0.25*(n-1250000);
+	            tax+=0.20*(1250000-1000000);
+	            tax+=0.15*(1000000-750000);
+	            tax+=0.10*(750000-500000);
+	            tax+=0.05*(500000-250000);
+	        }else if(n>1500000)
+	        {
+	            tax+=0.30*(n-1500000);
+	            tax+=0.25*(1500000-1250000);
+	            tax+=0.20*(1250000-1000000);
+	            tax+=0.15*(1000000-750000);
+	            tax+=0.10*(750000-500000);
+	            tax+=0.05*(500000-250000);
+	        }
+	        System.out.println(n-tax);
+	    }
+	}
+}
