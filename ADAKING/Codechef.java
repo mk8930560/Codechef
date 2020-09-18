d46156bc39cfdc8dba53d55dde10e5d7b8fc8a4d
mk8930560 <parasermanish@gmail.com>
2020-09-18 23:36:50 +0530

    Ada King

---
+++ ADAKING/Codechef.java
@@ -0,0 +1,40 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    while(t-->0)
+	    {
+	        int n=sc.nextInt();
+	        for(int i=1;i<=8;i++)
+	        {
+	            for(int j=1;j<=8;j++)
+	            {
+	                if(i==1 && j==1)
+	                {
+	                    System.out.print("O");
+	                    n--;
+	                }
+	                else if(n!=0)
+	                {
+	                    System.out.print(".");
+	                    n--;
+	                }
+	                else
+	                    System.out.print("X");
+	            }
+	            System.out.println();
+	        }
+	        System.out.println();
+	    }
+	}
+}
