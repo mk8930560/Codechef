853eec6963633fa929e4a2a7b49b06df5bfb2b5c
mk8930560 <parasermanish@gmail.com>
2021-01-06 10:39:37 +0530

    The Begining Era Of Cyberverse

---
+++ CYBV/Codechef.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    while(t-->0)
+	    {
+	        int child = sc.nextInt();
+	        int weapons = sc.nextInt();
+	        int min=weapons/child;
+	        System.out.println(min);
+	    }
+	}
+}
