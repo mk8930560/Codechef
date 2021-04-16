19830b579c56ce72a75f412dac60e5d74bc31d46
mk8930560 <parasermanish@gmail.com>
2021-04-16 14:53:59 +0530

    A - Save Water Save Life

---
+++ SAVWATER/Codechef.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    BufferedReader ob=new BufferedReader(new InputStreamReader(System.in));
+	    int t=Integer.parseInt(ob.readLine());
+	    while(t-->0)
+	    {
+	        String s[]=ob.readLine().split(" ");
+	        int totalHouse=Integer.parseInt(s[0]);
+	        int x=Integer.parseInt(s[1]);
+	        int y=Integer.parseInt(s[2]);
+	        int C=Integer.parseInt(s[3]);
+	        int answer=totalHouse*(x+(y/2));
+	        if(answer<=C)
+	            System.out.println("YES");
+	        else
+	            System.out.println("NO");
+	    }
+	}
+}
