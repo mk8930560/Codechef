daf54337c7dd61cd592482f744d3ee51fa275721
mk8930560 <parasermanish@gmail.com>
2021-05-07 16:16:41 +0530

    Solubility

---
+++ SOLBLTY/Codechef.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    BufferedReader ob=new BufferedReader(new InputStreamReader(System.in));
+	    int t=Integer.parseInt(ob.readLine());
+	    while(t-->0)
+	    {
+	        String s[]= ob.readLine().split(" ");
+	        
+	        int X = Integer.parseInt(s[0]);
+	        int A = Integer.parseInt(s[1]);
+	        int B = Integer.parseInt(s[2]);
+	        
+	        int answer = A+(100-X)*B;
+	        
+	        System.out.println(answer*10);
+	        
+	    }
+	}
+}
