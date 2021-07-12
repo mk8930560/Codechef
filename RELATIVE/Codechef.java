17d476fb26f62f0706c8b6a94422488fc40c0155
mk8930560 <parasermanish@gmail.com>
2021-07-12 20:26:24 +0530

    Relativity

---
+++ RELATIVE/Codechef.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    BufferedReader ob=new BufferedReader(new InputStreamReader(System.in));
+	    int t=Integer.parseInt(ob.readLine());
+	    while(t-->0)
+	    {
+	        String s[]= ob.readLine().split(" ");
+	        
+	        int g = Integer.parseInt(s[0]);
+	        int c = Integer.parseInt(s[1]);
+	        
+	        System.out.println((c*c)/(2*g));
+	        
+	    }
+	}
+}
